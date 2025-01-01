cafbf8f3e458257dd4039bddf0f9cfaa6f08ffef
DKULena <32173057@dankook.ac.kr>
2025-01-01 21:30:25 +0900

    add: TodoList using React

---
+++ src/components/List.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+
+export default function List({ todos, onUpdate, onDelete }) {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) =>
+      todo.content.toLowerCase().includes(search.toLocaleLowerCase())
+    );
+  };
+
+  const filteredTodos = getFilteredData();
+
+  return (
+    <div className="List">
+      <h4>Todo List 📘</h4>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        placeholder="검색어를 입력하세요"
+      />
+      <div className="todos_wrapper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+}
